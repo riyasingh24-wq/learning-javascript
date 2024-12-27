f9a70b9e68d51266c5370e571d84661f0ce711c0
Riya <Riya@riyasingh.com>
2024-12-27 22:13:27 +0530

    add the summary file

--- datatype-summary.js
+++ datatype-summary.js
@@ -18,8 +18,47 @@ const bigNumber = 7687628729792996877n
 // Reference (Non-Primitive)
 
 // Array, Object, Function
-const heros = ["Shaktiman","Naagraj","Doga"];
+/* const heros = ["Shaktiman","Naagraj","Doga"];
 let myObj = {
-      name : "riya",
+      name : "riya", 
       age : 20
+}  */
+
+// const gameName = new String('roxyrow')
+
+const myFunction = function(){
+      console.log("hello world");
+      
 }
+
+console.log(typeof null); // Check every typeof function or datatypes 
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive)  , Heap (Non-Primitive)
+
+let myYoutubename = "riyasingh@dotcom"
+
+let anothername = myYoutubename;
+anothername = "riya@dotcom"                     //Stack memory
+console.log(myYoutubename);
+console.log(anothername);
+
+
+// Heap
+
+let userOne = {
+      email: "user@gmail.com",
+      UPI: "user@ybl"
+}
+
+let userTwo = userOne;
+
+userTwo.email = "riya@gmail.com"
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
+
+
