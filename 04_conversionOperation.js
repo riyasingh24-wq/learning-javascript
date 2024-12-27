f3ffb11658b77723ac1dbbf78297293fe9500ab0
Riya <Riya@riyasingh.com>
2024-12-27 16:59:54 +0530

    add some files

---
+++ 04_conversionOperation.js
@@ -0,0 +1,90 @@
+let score = "riya"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+                        // "33" => 33
+                        // 33abc => NaN
+                        // true => 1; false => 0
+
+                        // "" => 0
+                        // "riya" => NaN
+
+
+let isLoggedIn = "riya"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false
+// ""  => false
+// "riya" => true
+
+let somenumber = 33
+
+let stringNumber = String(somenumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+/******************************************* Operation  *****************************************************/
+
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1 = "hello"
+let str2 = " riya"
+let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1"+2)
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log((3 + 4)* 5 % 3);
+
+console.log(+true);
+console.log(+"");
+
+let num1 , num2 , num3
+
+num1 = num2 = num2 + num3
+num1 = num2 = num3 = 2 + 2
+
+
+// prefix and post fix mdn JS docs read
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ datatype-summary.js
@@ -0,0 +1,25 @@
+// # Primitive
+
+// 7 types : String, Number, Boolean, bigInt, Undefined, null, Symbol
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null 
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+const bigNumber = 7687628729792996877n
+
+// Reference (Non-Primitive)
+
+// Array, Object, Function
+const heros = ["Shaktiman","Naagraj","Doga"];
+let myObj = {
+      name : "riya",
+      age : 20
+}
