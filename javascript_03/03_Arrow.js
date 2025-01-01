03c19335205b521f1807277b67cb34b032827ba7
Riya <Riya@riyasingh.com>
2025-01-01 20:46:30 +0530

    Changes in file

---
+++ javascript_03/03_Arrow.js
@@ -0,0 +1,9 @@
+const user = {
+      username: "Riya",
+      price: 999,
+
+      WelcomeMessage: function(){
+            console.log(`${this.username}, Welcome to the Website`);
+            
+      }
+}
