7d64bcd727d9f1be5cdcf9f8556a908d89bae583
Riya <Riya@riyasingh.com>
2025-01-02 23:40:45 +0530

    add the arrow file

--- javascript_03/03_Arrow.js
+++ javascript_03/03_Arrow.js
@@ -4,6 +4,17 @@ const user = {
 
       WelcomeMessage: function(){
             console.log(`${this.username}, Welcome to the Website`);
+            // console.log(this);
             
       }
 }
+// user.WelcomeMessage()
+// user.username = "Sam"
+// user.WelcomeMessage()
+// console.log(this);
+
+function chai(){
+      let username = "riya"
+      console.log(this.username);
+}
+chai()
